94e4f3f3fb1ce83dbb4d755468c462937aafadfd
Juan Fernando <jfdelarosa@outlook.com>
2015-01-17 19:37:25 -0600

    Added a simple menu on /admin

--- admin/index.php
+++ admin/index.php
@@ -2,8 +2,19 @@
 include('../includes/config.inc.php');
 include('template/header.inc.php');
 $p = $_GET['p'];
-if(in_array($p, array('agregar', 'editar', 'eliminar', 'login'))){
-   include($p.'.php');
+if(isset($p)){
+   if(in_array($p, array('agregar', 'editar', 'eliminar', 'login'))){
+      include($p.'.php');
+   }
+}else{
+?>
+Bienvenido a la administración
+<ul>
+   <li><a href="index.php?p=agregar">Agregar entrada</a></li>
+   <li><a href="index.php?p=ver">Ver entradas</a></li>
+   <li><a href="index.php?p=login&logout=1">Salir</a></li>
+</ul>
+<?php
 }
 include('template/footer.inc.php');
 ?>
